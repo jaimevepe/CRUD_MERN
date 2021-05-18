7ff1809866235f03d576b758c597ba9a21242ba5
jaimevepe <jaime.velazquez@g.austincc.edu>
2021-05-18 16:12:33 -0500

    Finished MERN APP

--- client/src/App.js
+++ client/src/App.js
@@ -7,21 +7,60 @@ function App() {
 
   const [name, setName] = useState("");
   const [age, setAge] = useState(0);
-  const [friendList, setFriendList] = useState([])
+  const [description, setDescription] = useState("")
+  const [friendList, setFriendList] = useState([]);
 
+// SEND DATA To the BackEnd
   const addFriend = () => {
     axios.post("http://localhost:3001/addfriend", { // sending an Obj to the backend
       name: name,                                   // which matches the variables
       age: age,
-    })
-    .then(response => {
+      description: description
+    }).then((response) => {
       console.log(response)
+    })
+    .then((res) => {
+      setFriendList([...friendList, { _id: res.data._id, name: name, age: age, description: description }])
     })
     .catch(err => {
       console.log("Error with axios POST:", err)
+    });
+  }
+
+  // UPDATE DATA
+  const updateFriend = (id) => {
+    const newName = prompt("Enter new name:")
+    const newAge = prompt("Enter new age:")
+    const newDescription = prompt("Enter new description")
+
+    axios.put("http://localhost:3001/update", {
+      newName: newName,
+      newAge: newAge,
+      newDescription: newDescription,
+      id: id
+    })
+    .then(() => {
+      setFriendList(friendList.map((val) => {
+        return val._id === id ? { _id: id, name: newName, age: newAge, description: newDescription } : val
+      }))
+    })
+    .catch(err => {
+      console.log(err)
     })
   }
 
+  // DELETE FRIEND
+  const deleteFriend = (id) => {
+    axios.delete(`http://localhost:3001/delete/${id}`)
+    .then(() => {
+      setFriendList(friendList.filter((val) => {
+        return val._id !== id;
+      }))
+    })
+  }
+
+// GET DATA from the backend and display it 
+// in the front end using useEffect
   useEffect(() => {
     axios.get("http://localhost:3001/read")
     .then(response => {
@@ -30,7 +69,7 @@ function App() {
     .catch(err => {
       console.log("Error with axios GET:", err)
     })
-  }, [])
+  },[])
 
   return (
     <div className="App">
@@ -49,13 +88,37 @@ function App() {
             setAge(event.target.value)
           }}
           />
+          <input
+            type="textarea"
+            placeholder="Friend Description"
+            onChange={((event) => {
+              setDescription(event.target.value)
+            })}
+          />
 
           <button onClick={addFriend}>Add Friend</button>
       </div>
 
-      { friendList.map((val, index) => {
-        return <div key={index}> {val.name} {val.age} </div>
-      }) }
+      <div className="friend_list">
+        { friendList.map((val, index) => {
+          return (
+            <div key={index} className="friend_container">
+                  <div className="friend"> 
+                    <h3>Name: {val.name}</h3> 
+                    <h3>Age: {val.age} </h3>
+                    <h4>Description: {val.description}</h4>
+                  </div>
+                  <button onClick={() => {
+                    updateFriend(val._id)
+                  }} >Update</button>
+                  <button id="removebtn" onClick={() => {
+                    deleteFriend(val._id)
+                  }}>X</button>
+            </div>
+                )
+        }) }
+      </div>
+      
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -23,16 +23,17 @@ mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
 app.post('/addfriend', async (req, res) => {
     const name = req.body.name; // to access the bodydata on the axios post
     const age = req.body.age;
+    const description = req.body.description;
 
-    const friend = new FriendModel({ name: name, age: age });
-  try {
-      await friend.save()
-  } catch (error) {
-      console.log(error)
-  }
+    const friend = new FriendModel({ name: name, age: age, description: description });
+    try {
+        await friend.save()
+    } catch (error) {
+        console.log(error)
+    }
 })
 
-// READ DATA
+// GET DATA
 app.get('/read', async (req, res) => {
     FriendModel.find({}, (err, result) => {
         if(err){
@@ -41,6 +42,37 @@ app.get('/read', async (req, res) => {
             res.send(result)
         }
     })
+})
+
+// UPDATE DATA
+app.put('/update', async (req, res) => {
+    const newAge = req.body.newAge;
+    const newName = req.body.newName;
+    const newDescription = req.body.newDescription
+    const id = req.body.id
+
+    try {
+        await FriendModel.findById(id, (error, friendToUpdate) => {
+            friendToUpdate.age = Number(newAge)
+            friendToUpdate.name = String(newName)
+            friendToUpdate.description = String(newDescription)
+            friendToUpdate.save()
+        })
+    } catch (error) {
+        console.log(error)
+    }
+    res.send("Updated")
+})
+
+// DELETE DATA
+app.delete('/delete/:id', async ( req, res) => {
+    const id = req.params.id
+    try {
+        await FriendModel.findByIdAndRemove(id)
+        res.send("Item Deleted")
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 
